4870eebf068f69975e16cf88d77a56c1d77b70f4
Abdulrehman Omer <abdulrehman.omer001@gmail.com>
2026-08-10 16:45:01 -0400

    Working on Project 1

--- 01-Heart-Disease-Predictor/train.py
+++ 01-Heart-Disease-Predictor/train.py
@@ -21,4 +21,21 @@ print("Shape:", df.shape) # Shows how many rows and columns are in the dataset w
 print("Columns:", df.columns.tolist()) # Shows the names of all the columns. So we know what features we are working with.
 print(df.head()) # Shows the first 5 rows of the dataset. To check the data has been loaded correctly.
 
+## Data Exploration ##
 
+print("\nMissing values:") # Check if any data is missing # LABEL
+print(df.isnull().sum()) # Count missing values per column. We need zero 
+
+print("\nTarget distribution:") # Check how many sick vs healthy patients. # LABEL
+print(df['target'].value_counts()) # 0 = no disease, 1 = disease - should be around balanced, so that model learns fairly.\
+
+print("\nBasic statistics:") # Get mean, min, max and std for each column. # LABEL
+print(df.describe()) # Quick overview of all numerical features
+
+## Feature and Target Split ##
+
+X = df.drop('target', axis=1) # features - remove target column from the dataframe. The inputs into our model. axis=1 means drop the target column.
+y = df['target'] # target - what we want to predict
+
+print("Features shape:", X.shape) # should be (303, 13) aka 303 rows and 13 columns. A dataframe -> rows x column.
+print("Target shape:", y.shape) # should be (303,) aka y should have 303 values. this is called series. not a dataframe. will only show us 0 or 1 whether patient has disease or not. Series -> just rows.
